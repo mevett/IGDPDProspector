94b6f507ed9cf2cf05c7f1b0dfac8a8f57bbdd4c
“Bugs <mevett@emich.edu>
2017-10-29 16:13:35 -0400

    initial.  just before Parsing Information from Deck XML

---
+++ Assets/__Scripts/Prospector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;      // This will be used later in the project 
+using UnityEngine.UI;                   // This will be used later in the project   
+
+public class Prospector : MonoBehaviour {     
+	static public Prospector    S;       
+
+	[Header("Set in Inspector")]     
+	public TextAsset            deckXML;        
+
+	[Header("Set Dynamically")]     
+	public Deck                    deck;       
+
+	void Awake() {         
+		S = this; // Set up a Singleton for Prospector     
+	}       
+
+	void Start () {         
+		deck = GetComponent<Deck>(); // Get the Deck         
+		deck.InitDeck(deckXML.text); // Pass DeckXML to it    
+	} 
+}
