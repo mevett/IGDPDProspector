d77b881b86ea99964c1533a6ad91d90320fdbf92
“Bugs <mevett@emich.edu>
2017-10-30 12:41:40 -0400

    Creating CardProspector.  After step 4.  drawPile shows as full of nulls.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -14,8 +14,8 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set Dynamically")]     
 	public Deck                 deck; 
-	public Layout			layout;
-
+	public Layout				layout;
+	public List<CardProspector> drawPile;
 
 	void Awake() {         
 		S = this; // Set up a Singleton for Prospector     
@@ -33,5 +33,18 @@ public class Prospector : MonoBehaviour {
 //		} 
 		layout = GetComponent<Layout>();  // Get the Layout component
 		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to the new component for parsing
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
 	} 
+
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD) { 
+		List<CardProspector> lCP = new List<CardProspector>(); 
+		CardProspector tCP; 
+		foreach( Card tCD in lCD ) { 
+			tCP = tCD as CardProspector;                   // a 
+			lCP.Add( tCP ); 
+		} 
+		return( lCP ); 
+	} 
+
 }
